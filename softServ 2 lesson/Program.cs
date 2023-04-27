f6f7b06c5d43ebfd527656215e23f0b98b581158
BudX_ol75@student.itstep.org <BudX_ol75@student.itstep.org>
2023-04-27 11:58:17 +0300

    Добавьте файлы проекта.

---
+++ softServ 2 lesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace softServ_2_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
